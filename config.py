aef9e39808809d50130e0a174e02ea1ffdd1fdeb
nishan1616 <157087473+nishan1616@users.noreply.github.com>
2024-03-21 19:43:48 -0700

    Update config.py

--- config.py
+++ config.py
@@ -17,17 +17,32 @@ def get_config():
         "experiment_name": "runs/tmodel"
     }
 
-def get_weights_file_path(config, epoch: str):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
-    model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
+def get_weights_file_path(config, epoch: str, colab=False):
+    if colab:
+        model_folder = f"{config['model_folder']}"
+        model_filename = f"{config['model_basename']}{epoch}.pt"
+        return str(Path('.') / model_folder / model_filename)
+    else:
+        model_folder = f"{config['datasource']}_{config['model_folder']}"
+        model_filename = f"{config['model_basename']}{epoch}.pt"
+        return str(Path('.') / model_folder / model_filename)
 
 # Find the latest weights file in the weights folder
-def latest_weights_file_path(config):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
-    model_filename = f"{config['model_basename']}*"
-    weights_files = list(Path(model_folder).glob(model_filename))
-    if len(weights_files) == 0:
-        return None
-    weights_files.sort()
-    return str(weights_files[-1])
+def latest_weights_file_path(config, colab=False):
+    if colab:
+        model_folder = f"{config['model_folder']}"
+        model_filename = f"{config['model_basename']}*"
+        weights_files = list(Path(model_folder).glob(model_filename))
+        if len(weights_files) == 0:
+            return None
+        weights_files.sort()
+        return str(weights_files[-1])
+    
+    else:
+        model_folder = f"{config['datasource']}_{config['model_folder']}"
+        model_filename = f"{config['model_basename']}*"
+        weights_files = list(Path(model_folder).glob(model_filename))
+        if len(weights_files) == 0:
+            return None
+        weights_files.sort()
+        return str(weights_files[-1])
